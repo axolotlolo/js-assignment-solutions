79b37f08512a5555342f85551166243b3fae4900
axolotlolo <akhinsreedalla11@yahoo.com>
2026-02-22 11:35:07 +0530

    Add Question 6: Objects

---
+++ question6.js
@@ -0,0 +1,14 @@
+const user = {
+  name: "John Doe",
+  email: "john@example.com",
+  password: "secret123",
+  age: 30
+};
+
+console.log(user.name);
+
+user.email = "john.doe@newdomain.com";
+
+user.isAdmin = true;
+
+console.log(user);
