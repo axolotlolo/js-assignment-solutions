71a2d85a8a20261ed448a4af84179aa10a2519a2
axolotlolo <akhinsreedalla11@yahoo.com>
2026-02-22 11:35:05 +0530

    Add Question 3: Conditional Statements

---
+++ question3.js
@@ -0,0 +1,11 @@
+function checkEvenOdd(num) {
+  if (num % 2 === 0) {
+    console.log(`${num} is even`);
+  } else {
+    console.log(`${num} is odd`);
+  }
+}
+
+checkEvenOdd(7);
+checkEvenOdd(12);
+checkEvenOdd(25);
